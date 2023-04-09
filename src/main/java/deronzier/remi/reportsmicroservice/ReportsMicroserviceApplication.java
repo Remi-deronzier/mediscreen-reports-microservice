d7673f401523befc79822ed7b3ab1ceadc7c43f9
remax21 <deronzier.remi@gmail.com>
2023-04-09 22:48:43 +0200

    :tada: (First commit)

---
+++ src/main/java/deronzier/remi/reportsmicroservice/ReportsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package deronzier.remi.reportsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsMicroserviceApplication.class, args);
+	}
+
+}
